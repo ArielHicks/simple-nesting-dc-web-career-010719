f536f05d398bd993b7b4615d8adbaa5cd7ad347e
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-02 23:55:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -5,7 +5,6 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-					
         },
         :alan_kay => {
           :known_for => "Object Orientation",
@@ -16,6 +15,7 @@ def hopper
           :languages => ["C"]
 
         }
+				puts grace_hopper
      }
 
 end
